0e17eda43e95f6e3b96ff0b3e650860674cd89fb
daniel bannert <d.bannert@anolilab.de>
2016-01-14 00:29:36 +0100

    added all tests to Traverse

--- tests/TraverseTest.php
+++ tests/TraverseTest.php
@@ -12,6 +12,16 @@ class TraverseTest extends \PHPUnit_Framework_TestCase
         $this->traverse = new Traverse();
     }
 
+    public function testMap()
+    {
+        $this->assertEquals(
+            ['a' => 1, 'b' => 4],
+            $this->traverse->map(['a' => 1, 'b' => 2], function ($value, $key) {
+                return [$key => $value + 2];
+            })
+        );
+    }
+
     public function testFirst()
     {
         $array = [100, 200, 300];
@@ -21,6 +31,38 @@ class TraverseTest extends \PHPUnit_Framework_TestCase
         });
 
         $this->assertEquals(200, $value);
+
+        $value = $this->traverse->first($array, function ($key, $value) {
+            if ($key === $value) {
+                return true;
+            }
+        }, false);
+
+        $this->assertFalse($value);
+    }
+
+    public function testFilter()
+    {
+        $this->assertEquals(
+            [1 => 2, 3 => 2],
+            $this->traverse->filter([1, 2, 1, 2], function ($value, $key) {
+                if (2 === $value) {
+                    return true;
+                }
+            }, false)
+        );
+    }
+
+    public function testReject()
+    {
+        $this->assertEquals(
+            [1 => 2, 3 => 2],
+            $this->traverse->reject([1, 2, 1, 2], function ($value, $key) {
+                if (2 !== $value) {
+                    return true;
+                }
+            }, false)
+        );
     }
 
     public function testLast()
